b34a48d3023bccf5e3c8989f47322845b7930402
Luke31 <lukas.m.schmid@gmail.com>
2020-05-06 22:03:29 +0900

    fix flake

--- gluemotion/_gluemotion.py
+++ gluemotion/_gluemotion.py
@@ -12,10 +12,10 @@ class Gluemotion(object):
         Analyze Text in AWS Glue DynamicFrame.
 
         Adds following columns to the DynamicFrame:
-        - covid_mentioned e.g. True if text contains 'covid' or 'コロナ', else False
+        - covid_mentioned e.g. True if 'covid' or 'コロナ' in text, else False
 
         :param rec: DyanmicFrame to expand with covid_mentioned
-        :return: new dynamicFrame containing original text and new covid_mentioned-col
+        :return: new df containing original text and new covid_mentioned-col
         """
         text = rec[self.text_col].lower()
         has_covid = 'covid' in text or 'コロナ' in text
